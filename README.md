5de8395c89a94e2f886f588662def1f65ec229f5
Amine L'ch <aminelch95@gmail.com>
2019-12-04 10:44:07 +0100

    Ajout de Design pattern Command demo

---
+++ README.md
@@ -0,0 +1,10 @@
+#Design pattern 
+
+**Objectif**
+apprendre les bonnes pratiques de design pattern 
+
+**Outils**
+
+Vscoduim 
+javac 
+
---
+++ command/Light.java
@@ -0,0 +1,8 @@
+public class Light{
+  private boolean on;
+  public void switchOn(){
+    on = true;
+  }
+  public void switchOff(){
+    on = false;
+  }
